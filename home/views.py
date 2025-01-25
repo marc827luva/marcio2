f1653667131e359fbfdc7a0616d773ac353de9d7
marc827luva <marciolucas08305@gmail.com>
2025-01-25 11:34:55 -0300

    aqui

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("A view index funcionou, Wow!")
+
+def sobre(request):
+    return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim<h1>")
+
+def index(request):
+    return render(request,'index.html')
 
 # Create your views here.
--- pweb/urls.py
+++ pweb/urls.py
@@ -5,4 +5,5 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
+    path('sobre', views.sobre,name='sobre'),
 ]
