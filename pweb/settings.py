a1623922a6d0aa3b10ed98168e5b9d76894c407b
marc827luva <marciolucas08305@gmail.com>
2025-02-03 11:52:36 -0300

    vercel

--- pweb/settings.py
+++ pweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t)*3vmx@26@u-p(oj)u_mj8a1g+w9kc@tghzpx2&=+k6#u-0+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vencel.app']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
--- pweb/urls.py
+++ pweb/urls.py
@@ -5,6 +5,7 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
+    path('', views.index,name='index'),
     path('sobre', views.sobre,name='sobre'),
     path('contato', views.contato,name='contato'),
     path('ajudar', views.ajudar,name='ajudar'),
